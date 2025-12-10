fb7883ebbcef0b68c1cd67caea2c010627155eef
Baranatalii <baranatalii@BARANs-MacBook-Air.local>
2025-12-10 02:31:38 -0500

    Exercise 5

--- app.js
+++ app.js
@@ -56,15 +56,15 @@ Complete the exercise in the space below:
 
 const isCharAVowel = (char) => {
   const lowerChar = char.toLowerCase();
-  
+
   return (
-    lowerChar === 'a' ||
-    lowerChar === 'e' ||
-    lowerChar === 'i' ||
-    lowerChar === 'o' ||
-    lowerChar === 'u'
+    lowerChar === "a" ||
+    lowerChar === "e" ||
+    lowerChar === "i" ||
+    lowerChar === "o" ||
+    lowerChar === "u"
   );
-}
+};
 
 console.log("Exercise 3 Result:", isCharAVowel("a"));
 
@@ -81,9 +81,24 @@ Complete the exercise in the space below:
 */
 
 const generateEmail = (name, domain) => {
-  return name + '@' + domain;
-}
+  return name + "@" + domain;
+};
 
-console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
+console.log("Exercise 4 Result:", generateEmail("johnsmith", "example.com"));
 
+/*
+Exercise 5: greetUser()
 
+Define a function called greetUser. It should take a name and a 
+time of day (morning, afternoon, evening) and return a personalized 
+greeting.
+
+Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
+
+Complete the exercise in the space below:
+*/
+const greetUser = (name) => {
+  return "Hello, " + name + "!";
+};
+
+console.log("Exercise 5 Result:", greetUser("Sam", "morning"));
